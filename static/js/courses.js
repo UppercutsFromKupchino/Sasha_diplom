7742859036807b745bc2373757b01cb5f801e052
UppercutsFromKupchino <mitya.kalmykov.01@mail.ru>
2023-05-31 00:34:45 +0300

    courses start

---
+++ static/js/courses.js
@@ -0,0 +1,14 @@
+Vue.component('courses-body', {
+    template: `<div></div>`,
+    data: function() {
+        return {
+
+        }
+    },
+    methods: {
+
+    }
+});
+var vc = new Vue({
+    el: '#courses'
+});
